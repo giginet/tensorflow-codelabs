6e624e5b5879a63797e2b3e13be7331d4aa74221
Kohki Miki <koki-miki@cookpad.com>
2019-05-08 14:15:40 -0700

    Complete Lab2

--- tensorflow2/fashion.py
+++ tensorflow2/fashion.py
@@ -14,3 +14,15 @@ def draw():
 
 training_images = training_images / 255.0
 test_images = test_images / 255.0
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(), 
+                                    tf.keras.layers.Dense(128, activation=tf.nn.relu), 
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+model.compile(optimizer = tf.train.AdamOptimizer(),
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(training_images, training_labels, epochs=5)
+
+model.evaluate(test_images, test_labels)
